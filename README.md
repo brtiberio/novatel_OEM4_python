964bae0ac41908c3a2324f1632db0c4cc3666038
Bruno Tibério <brtiberio@gmail.com>
2018-07-24 15:57:54 +0100

    A few corrections in code. Not all backward compatible.
    Moved from optoparse to argparse module.
    Changed Queue to make it compatible with python3 queue. Backwards compatibility is maintained.
    Restructured default location. Moved from Lib folder to base path.
    Moved examples to proper folder. This cause backwards compatibility problems. On import, replace 'import Lib.NovatelOEM4' with simply 'import NovatelOEM4'

--- README.md
+++
@@ -1,6 +0,0 @@
-## NovatelOEM4 module ##
-
-This is an atempt to create a library to communicate with Novatel Flexpak G2 GPS device as part of a project.
-Is still a lot incomplete and embrionary
-
-Read the documentation at: [NovatelOEM4 Python](http://novatel-oem4-python.readthedocs.io/en/latest/)
--- examples/simple_test.py
+++ examples/simple_test.py
@@ -1,13 +1,20 @@
 import signal
-import Queue
 import threading
-import optparse
 import logging
 from time import sleep
-import lib.NovatelOEM4 as Novatel
+import sys
+# make it compatible with python 2.7
+try:
+    import queue
+except ImportError:
+    import Queue as queue
+
+sys.path.append('..')
+import NovatelOEM4 as Novatel
 
 
 def main():
+    import argparse
 
     def signal_handler(signal, frame):
         print('You pressed Ctrl+C!')
@@ -16,7 +23,7 @@ def main():
 
     def saveData(dataQueue, fileFP, exitFlag):
 
-        fileFP.write("Indice,Time,PSolStatus,X,Y,Z,stdX,stdY,stdZ,VSolStatus,VX,VY,VZ,stdVX,stdVY,stdVZ,VLatency,SolAge,SolSatNumber\n")
+        fileFP.write("Index,Time,PSolStatus,X,Y,Z,stdX,stdY,stdZ,VSolStatus,VX,VY,VZ,stdVX,stdVY,stdVZ,VLatency,SolAge,SolSatNumber\n")
         fileFP.flush()
         while(exitFlag.isSet() == False):
             if(dataQueue.empty() == False):
@@ -49,31 +56,51 @@ def main():
                 sleep(0.1)
         return
 
-    '''#########################################################################
-
-    Start of the main program
-
-    #########################################################################'''
+    # -------------------------------------------------------------------------
+    # Start of the main program
+    # -------------------------------------------------------------------------
     # add arguments options
-    parser = optparse.OptionParser(usage="usage: %prog [options] arg1")
-    parser.add_option("-f", "--file", action="store", type="string",
-                      dest="filename", default="./output.csv")
-    parser.add_option("--log", action="store", type="string",
-                      dest="log", default="output.log")
-    parser.add_option("--log-level", action="store", type="int",
-                      dest="logLevel", default=20)
+    parser = argparse.ArgumentParser(add_help=True,
+                                     description='Simple tester for Novatel library')
+    parser.add_argument('-f', '--file', action='store', type=str, dest="filename",
+                        default='./output.csv', help='file to save data')
+    parser.add_argument('--log', action='store', type=str, dest='log', default='output.log',
+                        help='log file to be used')
 
-    (opts, args) = parser.parse_args()
-    if len(args) > 3:
-        parser.error("incorrect number of arguments")
-        return
-    logging.basicConfig(filename=opts.log,
-                        level=opts.logLevel,
+    parser.add_argument("--log-level", action="store", type=str,
+                        dest="logLevel", default='info',
+                        help='Log level to be used. See logging module for more info',
+                        choices=['critical', 'error', 'warning', 'info', 'debug'])
+
+    args = parser.parse_args()
+
+    log_Level = {'error': logging.ERROR,
+                'debug': logging.DEBUG,
+                'info': logging.INFO,
+                'warning': logging.WARNING,
+                'critical': logging.CRITICAL
+                }
+
+    logging.basicConfig(filename=args.log,
+                        level=log_Level[args.logLevel],
                         format='[%(asctime)s] [%(threadName)-10s] %(levelname)-8s %(message)s',
                         filemode="w")
-    dataFile = opts.filename
+    # ---------------------------------------------------------------------------
+    # define a Handler which writes INFO messages or higher in console
+    # ---------------------------------------------------------------------------
+    console = logging.StreamHandler()
+    console.setLevel(log_Level[args.logLevel])
+    # set a format which is simpler for console use
+    formatter = logging.Formatter('%(name)-20s: %(levelname)-8s %(message)s')
+    # tell the handler to use this format
+    console.setFormatter(formatter)
+    # add the handler to the root logger
+    logging.getLogger('').addHandler(console)
+    # ---------------------------------------------------------------------------
+
+    dataFile = args.filename
     # create a queue for data input
-    dataQueue = Queue.Queue()
+    dataQueue = queue.Queue()
     # open file for saving data input
     myFile = open(dataFile, 'w')
     # create a flag to signal we want to exit program
@@ -85,14 +112,14 @@ def main():
     # prepare signal and handlers
     signal.signal(signal.SIGINT, signal_handler)
     gps = Novatel.Gps()
-    if (gps.begin(dataQueue) != 1):
+    if gps.begin(dataQueue) != 1:
         print("Not able to begin device properly... check logfile")
         return
     gps.setCom(baud=115200)
     # 0 Air 1 Land 2 foot
     # gps.setDynamics(2)
-    gps.askLog(trigger=2, period=0.1)
-    print ('Press Ctrl+C to Exit')
+    gps.askLog(trigger=2, period=0.05)
+    print('Press Ctrl+C to Exit')
     signal.pause()
     exitFlag.set()
     threadID.join()
